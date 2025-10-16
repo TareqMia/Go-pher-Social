e5222a83f225ec9d3a11d2bb0922ce75cef61d2d
Tareq <tareqmia81@gmail.com>
2025-10-15 21:19:34 -0400

    Set up db connection via postgress running in docker container.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gopher-social/internal/db"
 	"gopher-social/internal/env"
 	"gopher-social/internal/env/store"
 	"log"
@@ -11,13 +12,24 @@ func main() {
 	config := &config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
-	store := store.NewStorage(nil)
+
+	db, err := db.New(config.db.addr, config.db.maxOpenConns, config.db.maxIdleConns, config.db.maxIdleTime)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+
+	log.Println("DB connection established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: *config,
