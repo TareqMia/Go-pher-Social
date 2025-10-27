8d1ff8a789c9e3bbc9dd76d0b205a64634dbd720
Tareq <tareqmia81@gmail.com>
2025-10-27 07:35:42 -0400

    Added create post handler

---
+++ cmd/api/posts.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"gopher-social/internal/env/store"
+	"net/http"
+)
+
+type CreatePostPayload struct {
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+	var payload CreatePostPayload
+
+	if err := readJSON(w, r, &payload); err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	userId := 1
+	ctx := r.Context()
+
+	post := &store.Post{
+		Title:   payload.Title,
+		Content: payload.Content,
+		Tags:    payload.Tags,
+		// TODO: change after auth
+		UserId: int64(userId),
+	}
+
+	if err := app.store.Posts.Create(ctx, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
