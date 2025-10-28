d61fd05a4f6fc361788eb5e38d9282f1e1c1dcca
Tareq <tareqmia81@gmail.com>
2025-10-27 20:15:17 -0400

    Implemented getting post by id.

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"errors"
 	"gopher-social/internal/env/store"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
@@ -35,6 +39,35 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	idParam := chi.URLParam(r, "postId")
+
+	postId, err := strconv.ParseInt(idParam, 10, 64)
+
+	if err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	post, err := app.store.Posts.GetById(ctx, postId)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
